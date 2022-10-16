82131facc328e649459d14bee6af35501657245c
intero3 <cihanbatas@gmail.com>
2022-10-16 13:06:45 +0200

    Create-App

---
+++ ecommerce/frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">E-COMMERCE</a>
+      </header>
+      <main>
+        products
+      </main>
+    </div>
+  );
+}
+
+export default App;
